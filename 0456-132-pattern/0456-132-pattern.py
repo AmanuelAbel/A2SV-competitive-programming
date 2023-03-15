3000035accff5fbf8c4d1e7581d09da5ebd21792
AmanuelAbel <88858025+AmanuelAbel@users.noreply.github.com>
2023-03-15 16:36:42 +0300

    Time: 404 ms (28.37%), Space: 32.1 MB (33.43%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -3,13 +3,13 @@ class Solution:
         stack = []
         var = float("inf")
         for i in range(len(nums)):
-            while stack and nums[stack[-1][0]] < nums[i]:
+            var = min(var,nums[i])
+            while stack and nums[stack[-1][0]] <= nums[i]:
                 stack.pop()
             if stack:
-                if nums[i] > stack[-1][1] and nums[stack[-1][0]] > nums[i]:
+                if nums[i] > stack[-1][1]:
                     return True
             stack.append([i,var])
-            var = min(var,nums[i])
         return False    
            
         
