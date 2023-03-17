1736391ff450de73db0abf6d500c7991ccb13970
AmanuelAbel <88858025+AmanuelAbel@users.noreply.github.com>
2023-03-17 10:40:44 +0300

    added subsets.python

--- subsets.py
+++ subsets.py
@@ -1,14 +1,9 @@
 class Solution:
-    def helper(self,i,subsets,combs,nums):
-        if i >= len(nums):
-            subsets.append(combs.copy())
-            return 
-        combs.append(nums[i])
-        self.helper(i+1,subsets,combs,nums)
-        combs.pop()
-        self.helper(i+1,subsets,combs,nums)
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        subsets =[]
-        combs  =[]
-        self.helper(0,subsets,combs,nums)
-        return subsets
+        ans = [[]]
+        for i in range(len(nums)):
+            m = len(ans)
+            for j in range(m):
+                res = ans[j] + [nums[i]]
+                ans.append(res)
+        return ans
