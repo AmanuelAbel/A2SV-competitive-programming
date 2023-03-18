6fd7b5f992cb13ee5ed4c024440f29c5f238c524
AmanuelAbel <88858025+AmanuelAbel@users.noreply.github.com>
2023-03-18 19:53:50 +0300

    added predict-the-winner.python

--- predict-the-winner.py
+++ predict-the-winner.py
@@ -1,31 +1,8 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        def winner(start,end,turn):
-            if start == end:
-                if turn:
-                    return [nums[start],0]
-                else:
-                    return [0,nums[start]]
-            choice1 = winner(start + 1,end,not turn )
-            choice2 = winner(start,end - 1 ,not turn)
-            if turn:
-                if choice1[0] + nums[start] > choice2[0] + nums[end]:
-                    choice1[0] += nums[start]
-                    return choice1
-                else:
-                    choice2[0] += nums[end]
-                    return choice2
-            else:
-                if choice2[1] + nums[end] > choice1[1] + nums[start]:
-                    choice2[1] += nums[end]
-                    return choice2
-                else:
-                    choice1[1] += nums[start]
-                    return choice1
-        player1, player2 = winner(0, len(nums) -1, True)
-        print(player1)
-        if player1 >= player2:
-            return True
-        else:
-            return False
-        return True
+        def winner(start,end):
+            if start > end:
+                return 0
+            return max(nums[start] - winner(start+1,end),nums[end] -winner(start,end-1))
+        z =winner(0,len(nums)-1)
+        return z >= 0
