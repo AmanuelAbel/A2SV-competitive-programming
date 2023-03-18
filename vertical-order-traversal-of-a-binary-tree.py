267c106315165a017f21767f504ceb46b5c67db4
AmanuelAbel <88858025+AmanuelAbel@users.noreply.github.com>
2023-03-18 20:02:31 +0300

    added vertical-order-traversal-of-a-binary-tree.python

---
+++ vertical-order-traversal-of-a-binary-tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import defaultdict as dd
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        ans = dd(list)
+        queue = deque([(root.val, 0, root)])
+        while queue:
+            size = len(queue)
+            nq = []
+            while size:
+                val, col, node = queue.popleft()
+                ans[col].append(val)
+                if node.left: nq.append((node.left.val, col - 1, node.left))
+                if node.right: nq.append((node.right.val, col + 1, node.right))
+                size -= 1
+            queue = deque(sorted(nq, key = lambda x: x[0]))
+        res = []
+        for k in sorted(ans):
+            res.append(ans[k])
+        return res
