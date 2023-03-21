45e985a0a6150f52257e532a27168270e5581669
AmanuelAbel <88858025+AmanuelAbel@users.noreply.github.com>
2023-03-21 19:03:03 +0300

    added h-index-ii.python

---
+++ h-index-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        left = 0
+        n = len(citations)
+        right = len(citations) - 1
+        while left <= right:
+            mid = (right+left) // 2
+            if len(citations) - mid > citations[mid]:
+                left = mid + 1
+            elif len(citations)- mid < citations[mid]:
+                right = mid - 1
+            else:
+                return citations[mid]
+        return len(citations) - left
