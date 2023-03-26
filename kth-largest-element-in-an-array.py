073068d2fc0e6c4fe07a7a91c8d3ca94b370ffee
AmanuelAbel <88858025+AmanuelAbel@users.noreply.github.com>
2023-03-26 12:12:52 +0300

    added kth-largest-element-in-an-array.python

--- kth-largest-element-in-an-array.py
+++ kth-largest-element-in-an-array.py
@@ -1,35 +1,21 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        def sortArray(nums: List[int]) -> List[int]:
-            def mergeSort(left, right, arr):
-                if left == right:
-                    return [arr[left]]
-                mid = left + (right - left) // 2
-                left_half = mergeSort(left, mid, arr)
-                right_half = mergeSort(mid + 1, right, arr)
-    
-                return merge(left_half, right_half)
-
-            def merge(left_half, right_half):
-                idx1 = 0
-                idx2 = 0
-                merged = []
-                while idx1 < len(left_half) and idx2 < len(right_half):
-                    if left_half[idx1] < right_half[idx2]:
-                        merged.append(left_half[idx1])
-                        idx1 += 1
-                    else:
-                        merged.append(right_half[idx2])
-                        idx2 += 1
-
-                if idx1 < len(left_half):
-                    merged += left_half[idx1:]
-
-                if idx2 < len(right_half):
-                    merged += right_half[idx2:]
-
-                return merged
-
-            return mergeSort(0, len(nums)-1, nums)
-        nums = sortArray(nums)
-        return nums[-k]
+        positive = [[] for _ in range(10001)]
+        negative = [[] for _ in range(10001)]
+        for num in nums:
+            if num >= 0:
+                positive[num].append(num)
+            else:
+                negative[num].append(num)
+        array = []
+        for i in negative:
+            if len(i) == 0:
+                continue
+            else:
+                array += i
+        for i in positive:
+            if len(i) == 0:
+                continue
+            else:
+                array += i
+        return array[-k]
