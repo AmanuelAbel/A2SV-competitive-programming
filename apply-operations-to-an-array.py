93215e375e58e50b6e025cf487df76088fcceac0
AmanuelAbel <88858025+AmanuelAbel@users.noreply.github.com>
2023-03-27 17:36:33 +0300

    added apply-operations-to-an-array.python

---
+++ apply-operations-to-an-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        for i in range(1,len(nums)):
+            if nums[i-1] == nums[i]:
+                nums[i-1] = nums[i-1] * 2
+                nums[i] = 0
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                if nums[l] == 0:
+                    nums[l],nums[r] = nums[r],nums[l]
+                l+=1
+        return nums
