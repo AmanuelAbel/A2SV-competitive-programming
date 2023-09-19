71a155e22d107303fffa15619d031eaf230b39a4
AmanuelAbel <88858025+AmanuelAbel@users.noreply.github.com>
2023-09-19 03:16:54 -0400

    added 2-keys-keyboard.python

--- 2-keys-keyboard.py
+++ 2-keys-keyboard.py
@@ -1,14 +1,19 @@
 class Solution:
+    def isprime(num):
+            for n in range(2,int(num**0.5)+1):
+                if num%n==0:
+                    return False
+            return True
+    nums = defaultdict(int)
+    nums[1] = 0
+    nums[2] = 2
+    for i in range(3,1001):
+        if isprime(i):
+            nums[i] = i
+        else:
+            for z in range (2,i):
+                if i // z in nums and (i//z) * z == i:
+                    nums[i] = nums[i//z] + nums[z]
+                    break
     def minSteps(self, n: int) -> int:
-        res = []
-        def prime(n,factorization):
-            d = 2
-            while d * d <= n:
-                while n % d == 0:
-                    factorization.append(d)
-                    n //= d
-                d += 1
-            if n > 1:
-                factorization.append(n)
-        prime(n,res)
-        return sum(res)
+        return self.nums[n]
