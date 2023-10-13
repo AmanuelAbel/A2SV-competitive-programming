98855645e5a91be018f294020f86940d91bcbea8
AmanuelAbel <88858025+AmanuelAbel@users.noreply.github.com>
2023-10-13 18:24:38 +0300

    added distribute-coins-in-binary-tree.python

---
+++ distribute-coins-in-binary-tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+        result=0
+        def dfs(root):
+            nonlocal result
+            if not root:
+                return 0
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+            result += abs(left) + abs(right) 
+
+            return left + right + root.val -1
+        dfs(root)
+        return result
